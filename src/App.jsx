16d6e9839ad80958ea67abecd3cb9fbc30864b78
henok <ekrammehamed13@gmail.com>
2026-06-06 12:33:44 +0300

    modified:   src/App.jsx modified:   src/components/Header.jsx 	new file:   src/pages/Blog.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { Routes, Route, ServerRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
 import Tracking from "./pages/Tracking";
-import Checkout from "./pages/Checkout"; // Make sure Checkout is imported if you or your team added it
+import Checkout from "./pages/Checkout";
 import Services from "./pages/Services";
+import Blog from "./pages/Blog";
 
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-white w-full">
       <Header />
+
       <div className="flex-1 w-full">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -19,12 +22,14 @@ function App() {
           <Route path="/cart" element={<Cart />} />
           <Route path="/checkout" element={<Checkout />} />
 
-          {/* Keep both your custom tracking routes */}
           <Route path="/tracking" element={<Tracking />} />
           <Route path="/tracking/:orderId" element={<Tracking />} />
-           <Route path="/services" element={<Services />} /> 
+
+          <Route path="/services" element={<Services />} />
+          <Route path="/blog" element={<Blog />} />
         </Routes>
       </div>
+
       <Footer />
     </div>
   );
