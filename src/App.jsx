e27a218f9991e027ee4f0dec3868393c872ecdd4
danices25 <danionline519@gmail.com>
2026-06-06 10:21:04 +0300

    modified:   src/App.jsx 	new file:   src/pages/Services.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, ServerRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
 import Cart from "./pages/Cart";
+import Services from "./pages/Services";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<Products />} />
           <Route path="/cart" element={<Cart />} />
+           <Route path="/services" element={<Services />} /> 
         </Routes>
       </div>
       <Footer />
